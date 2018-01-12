86bcf182cafec7edfcf0b64b3cfa72632ec95b36
gentok <gento.badger@gmail.com>
2018-01-12 03:11:10 -0800

    update

--- R/plot_coef.R
+++ R/plot_coef.R
@@ -1,39 +1,6 @@
 # Following variables are global
 globalVariables(c("CF", "lower", "overlap", "upper", "vars"))
 
-#' Default \code{ggplot} theme for \code{viseffect} package.
-#'
-#' @importFrom ggplot2 theme
-#' @importFrom ggplot2 element_text
-#' @importFrom ggplot2 element_blank
-#' @importFrom ggplot2 element_line
-#' @importFrom ggplot2 element_rect
-#'
-#' @export
-viseffect.theme <- function(){
-  gt <- theme(text = element_text(size=10, colour="black"), # General Text Setting
-        axis.text.x=element_text(colour="black"), # x axis labels text
-        axis.text.y=element_text(colour="black"), # y axis labels text
-        axis.title.x=element_text(size=11,vjust=-1.5), # x axis title text
-        axis.title.y=element_text(size=11,vjust=1.5), # y axis title text
-        plot.title=element_text(size=11,lineheight=.8,face="bold",vjust=2,hjust=0.5), # plot title
-        plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"), # margins of the graph area *top, right, bottom, left
-        panel.grid.major.x = element_line(colour = "grey90", linetype=2), # x axis grid line (major)
-        panel.grid.major.y = element_line(colour = "grey90", linetype=2), # y axis grid line (major)
-        panel.grid.minor.x = element_line(colour = "grey90", linetype=2), # x axis grid line (minor)
-        panel.grid.minor.y = element_line(colour = "grey90", linetype=2), # y axis grid line
-        panel.background = element_rect(fill=NA, colour="black", size=0.5, linetype=1), # plot panel area setting
-        legend.background = element_rect(fill="white",colour="black"), # legend box setting
-        legend.position = "bottom", # position of the legend: c(x,y) OR "top","bottom","left","right"
-        legend.title=element_blank(), # Setting of the legend title
-        legend.margin = margin(t=0.01,r=0.2,b=0.1,l=0.1,"cm"), # margins for legend box
-        legend.key = element_rect(fill=NA,colour=NA), # legend keys settings
-        legend.key.width = unit(1.2, "cm"), # size(width) of the legend keys
-        strip.background = element_rect(fill=NA,colour=NA) # Setting of the facetted title area
-        )
-  return(gt)
-}
-
 #' Plotting Coefficients
 #'
 #' @description Drawing the coefficient plots from either model result or coefficient table. Borrowed the idea of odds ratio plotting from the code in \url{http://www.surefoss.org/dataanalysis/plotting-odds-ratios-aka-a-forrestplot-with-ggplot2/}.
@@ -42,6 +9,8 @@ viseffect.theme <- function(){
 #' @param direct Import the result directly from the table (boulean). If \code{FALSE} (default), coefficients imported from estimated model object (\code{\link[stats]{coef}} and \code{\link[stats]{confint}} must be applicable). If \code{TRUE}, coefficients imported directly from coefficients table (rows=variables, columns=(coefficient, lower CI, upper CI), row names = variable names).
 #' @param order.variable Order of coefficients in the plot. \code{"original"} (default) preserves the original order of the variable. \code{"coeforder"} plots by the discending order of the coefficient size. \code{"asis"} use the default \code{ggplot} setting.
 #' @param odds Use odds ratio instead of coefficient in the output (boulean). The default is \code{FALSE}. If \code{TRUE}, the exponent of the coefficients will be plotted.
+#' @param show.confint Show confidence interval (boulean). The default is \code{TRUE}.
+#' @param percent.confint The percentage used for confidence interval (numeric: 0-100). The default is \code{95}.
 #' @param drop.intercept Drop the intercept from the plot (boulean). If \code{FALSE} (default), intercept included in the plot.
 #' @param drop.intercept.names The name(s) of intercept (character/character vector). Needed if \code{drop.intercept} is \code{TRUE}. This is used to identify and eliminate intercept variables from the output. Default value is \code{"(Intercept)"}.
 #' @param drop.variable.names The name(s) of additional variables to drop (character/character vector) from the ouput. The default is \code{NULL}.
@@ -55,7 +24,7 @@ viseffect.theme <- function(){
 #' @param overlap.gapwidth The gap between overlapped ouputs (number). The default value is \code{0.5}.
 #' @param overlap.shape.index The index of shapes for overlapped point ouputs. Must be in the same length as \code{overlap.x.list} + 1. The first element of the vector is the shape for \code{x}, then from the second element, the order must correspond with the order in \code{overlap.x.list}. If \code{NULL}, \code{point.shape} is applied to all classes.
 #' @param overlap.linetype.index The index of line types for overlapped confidence interval ouputs. Must be in the same length as \code{overlap.x.list} + 1. The first element of the vector is the shape for \code{x}, then from the second element, the order must correspond with the order in \code{overlap.x.list}. If \code{NULL}, the number corresponding with the order is assigned to each class.
-#' @param overlap.legend.position The position of the legend for overlapping classess. See \code{legend.position} in ggplot theme for possible values. The default is \code{"top"}.
+#' @param overlap.legend.position The position of the legend for overlapping classess. See \code{legend.position} in ggplot theme for possible values. The default is \code{"bottom"}.
 #' @param facet.category.names The categories of variables (factor). If not \code{NULL}, the output provides the separate panels for variables in each category. The length of the vector must much with the number of variables in \code{x} (This is considered BEFORE the application of \code{drop.intercept} and \code{drop.variable.names}. Just insert \code{NA} for those variables to be dropped.).
 #' @param facet.names.location The location of facetted category names. Either \code{"left"} or \code{"right"}. The default is \code{"left"}.
 #' @param facet.names.angle The angle of facetted category names (numeric). The default is \code{0} (horizontal).
@@ -65,9 +34,72 @@ viseffect.theme <- function(){
 #'
 #' @return \code{ggplot} object (axis and theme settings can be added later).
 #'
+#' @examples
+#' ## Load Data
+#' library(pscl)
+#' data(vote92)
+#'
+#' ## Recode Variables
+#' vote92$voteBush <- as.numeric(
+#'   factor(vote92$vote,levels=c("Clinton","Bush")))*1 - 1
+#' vote92$bushdis <- sqrt(vote92$bushdis)
+#' vote92$clintondis <- sqrt(vote92$clintondis)
+#'
+#' ## Estimate Logistic Regression
+#' fm <- formula(voteBush ~ dem + rep +
+#'                 clintondis + bushdis +
+#'                 persfinance + natlecon)
+#' m <- glm(fm, data = vote92,
+#'          family = binomial("logit"))
+#'
+#' ## Basic Graph
+#' plot_coef(m)
+#'
+#' ## Change X-axis to Odds Ratio
+#' plot_coef(m, odds = TRUE)
+#'
+#' ## Export the Coefficients Table First, and import directly
+#' cfci <- data.frame(cbind(coef(m), confint(m, level = 0.95)))
+#' plot_coef(cfci, direct = TRUE)
+#'
+#' ## Estimate Model by Male and Female Subset
+#' m_male <- glm(fm, data = vote92[vote92$female==0,],
+#'          family = binomial("logit"))
+#' m_female <- glm(fm, data = vote92[vote92$female==1,],
+#'               family = binomial("logit"))
+#'
+#' ## Overlap Subsetted Results
+#' plot_coef(m_male,
+#'           overlap.x.list = list(m_female),
+#'           overlap.class.names = c("Male", "Female"))
+#'
+#' ## Add Title and Custom Variable Names
+#' vn <- c("(Intercept)",
+#'         "Democrat","Republican",
+#'         "Ideological Distance from Clinton",
+#'         "Ideological Distance from Bush",
+#'         "Retrospective Personal Finance",
+#'         "Retrospective National Economy")
+#' plot_coef(m_male,
+#'           overlap.x.list = list(m_female),
+#'           overlap.class.names = c("Male", "Female"),
+#'           title = "Vote for Bush (1992)",
+#'           custom.variable.names = vn)
+#'
+#' ## Overlap the Third Model & Facet Variables by Category
+#' fn <- c("Constant",rep("Preference",4),rep("Evaluation",2))
+#' fn <- factor(fn,levels=unique(fn))
+#' plot_coef(m_male,
+#'           overlap.x.list = list(m_female, m),
+#'           overlap.class.names = c("Male", "Female", "All"),
+#'           facet.category.names = fn,
+#'           title = "Vote for Bush (1992)",
+#'           custom.variable.names = vn)
+#'
 #' @importFrom stats coef
 #' @importFrom stats confint
 #' @importFrom stats reorder
+#' @import MASS
 #' @importFrom ggplot2 theme
 #' @importFrom ggplot2 ggplot
 #' @importFrom ggplot2 aes
@@ -86,12 +118,15 @@ viseffect.theme <- function(){
 #' @importFrom ggplot2 scale_linetype_manual
 #' @importFrom ggplot2 scale_x_discrete
 #' @importFrom ggplot2 facet_grid
+#' @import pscl
 #'
 #' @export
 plot_coef<-function(x,
                     direct=FALSE,
                     order.variable="original",
                     odds=FALSE,
+                    show.confint = TRUE,
+                    percent.confint = 95,
                     drop.intercept=FALSE,
                     drop.intercept.names = "(Intercept)",
                     drop.variable.names = NULL,
@@ -105,7 +140,7 @@ plot_coef<-function(x,
                     overlap.gapwidth = 0.5,
                     overlap.shape.index = NULL,
                     overlap.linetype.index = NULL,
-                    overlap.legend.position = "top",
+                    overlap.legend.position = "bottom",
                     facet.category.names = NULL,
                     facet.names.location = "left",
                     facet.names.angle = 0,
@@ -120,7 +155,7 @@ gtheme <- viseffect.theme() +
        panel.grid.minor.x = element_blank()) # x axis grid line (minor))
 
 ## Import Coefficients
-if (direct){
+if (direct==TRUE){
   tmp <- x
   originalx <- x
   varnames <- row.names(originalx)
@@ -129,26 +164,26 @@ if (direct){
     for(i in 1:length(overlap.x.list)){
       x <- rbind(x,overlap.x.list[[i]])
     }
+    tmp <- x
     varnames <- rep(row.names(originalx),length(overlap.x.list)+1)
     facetnames <- rep(facet.category.names,length(overlap.x.list)+1)
   }
-} else {
-  tmp <- data.frame(cbind(coef(x), confint(x)))
+} else if (direct==FALSE) {
+  pci <- percent.confint/100
+  tmp <- data.frame(cbind(coef(x), confint(x, level = pci)))
   originalx <- tmp
   varnames <- row.names(originalx)
   facetnames <- facet.category.names
   if (is.null(overlap.x.list)==FALSE) {
     for(i in 1:length(overlap.x.list)){
-      tmp <- rbind(tmp,data.frame(cbind(coef(overlap.x.list[[i]]), confint(overlap.x.list[[i]]))))
+      tmp <- rbind(tmp,data.frame(cbind(coef(overlap.x.list[[i]]), confint(overlap.x.list[[i]], level = pci))))
     }
     varnames <- rep(row.names(originalx),length(overlap.x.list)+1)
     facetnames <- rep(facet.category.names,length(overlap.x.list)+1)
   }
 }
 if (odds){
-  tmp <- exp(x)
-} else {
-  tmp <- x
+  tmp <- exp(tmp)
 }
 coefs <- tmp
 names(coefs)<-c('CF', 'lower', 'upper')
@@ -177,7 +212,7 @@ if (is.null(drop.variable.names)==FALSE) {
 if (is.null(custom.variable.names)==FALSE) {
   coefs$vars <- custom.variable.names
   if (is.null(overlap.x.list)==FALSE) {
-    coefs$vars <- rep(custom.variable.names, length(overlap.x.list))
+    coefs$vars <- rep(custom.variable.names, length(overlap.x.list)+1)
   }
 }
 
@@ -215,15 +250,21 @@ if (order.variable=="asis"){
 
 ## Intermediate Plot
 if (is.null(overlap.x.list)==TRUE) {
-  plotmid <- plotstart + geom_point(shape=point.shape, size=point.size) +
-  geom_errorbar(aes(ymin=lower, ymax=upper), linetype=confint.linetype, width=confint.height, size=confint.linewidth)
+  plotmid <- plotstart + geom_point(shape=point.shape, size=point.size)
+  if (show.confint==TRUE) {
+    plotmid <- plotmid +
+    geom_errorbar(aes(ymin=lower, ymax=upper), linetype=confint.linetype, width=confint.height, size=confint.linewidth)
+  }
 } else {
   plotmid <- plotstart +
   geom_point(aes(shape=overlap), size=point.size, position=position_dodge(width = -overlap.gapwidth)) +
-  geom_errorbar(aes(ymin=lower, ymax=upper, linetype=overlap), width=confint.height, size=confint.linewidth, position=position_dodge(width = -overlap.gapwidth)) +
   scale_shape_manual(name="Class",values=overlap.shapes)+
-  scale_linetype_manual(name="Class",values=overlap.linetypes) +
   theme(legend.position=overlap.legend.position)
+  if (show.confint==TRUE) {
+    plotmid <- plotmid +
+    geom_errorbar(aes(ymin=lower, ymax=upper, linetype=overlap), width=confint.height, size=confint.linewidth, position=position_dodge(width = -overlap.gapwidth)) +
+    scale_linetype_manual(name="Class",values=overlap.linetypes)
+  }
 }
 
 ## Intermediate Plot 2 (If Facetted)
@@ -245,14 +286,24 @@ if (is.null(facet.category.names)==FALSE) {
 
 ## Final Plot
 if (odds){
+  if (show.confint==TRUE) {
+    ylabtxt <- paste('Odds Ratio with ', percent.confint, '% Confidence Interval', sep="")
+  } else {
+    ylabtxt <- 'Odds Ratio'
+  }
   plotfin <- plotmid + scale_y_log10(breaks=ticks, labels = ticks) +
   scale_x_discrete() +
   geom_hline(yintercept = 1, linetype=2) +
-  labs(title = title, x = NULL, y = 'Odds Ratio')
+  labs(title = title, x = NULL, y = ylabtxt)
 } else {
+  if (show.confint==TRUE) {
+    ylabtxt <- paste('Coefficient with ', percent.confint, '% Confidence Interval', sep="")
+  } else {
+    ylabtxt <- 'Coefficient'
+  }
   plotfin <- plotmid + scale_x_discrete() +
   geom_hline(yintercept = 0, linetype=2) +
-  labs(title = title, x = NULL, y = 'Coefficient')
+  labs(title = title, x = NULL, y = ylabtxt)
 }
 
 ## Override Custom Themes
---
+++ R/viseffect.theme.R
@@ -0,0 +1,32 @@
+#' Default \code{ggplot} theme for \code{viseffect} package.
+#'
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_text
+#' @importFrom ggplot2 element_blank
+#' @importFrom ggplot2 element_line
+#' @importFrom ggplot2 element_rect
+#'
+#' @export
+viseffect.theme <- function(){
+  gt <- theme(text = element_text(size=10, colour="black"), # General Text Setting
+        axis.text.x=element_text(colour="black"), # x axis labels text
+        axis.text.y=element_text(colour="black"), # y axis labels text
+        axis.title.x=element_text(size=11,vjust=-1.5), # x axis title text
+        axis.title.y=element_text(size=11,vjust=1.5), # y axis title text
+        plot.title=element_text(size=11,lineheight=.8,face="bold",vjust=2,hjust=0.5), # plot title
+        plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"), # margins of the graph area *top, right, bottom, left
+        panel.grid.major.x = element_line(colour = "grey90", linetype=2), # x axis grid line (major)
+        panel.grid.major.y = element_line(colour = "grey90", linetype=2), # y axis grid line (major)
+        panel.grid.minor.x = element_line(colour = "grey90", linetype=2), # x axis grid line (minor)
+        panel.grid.minor.y = element_line(colour = "grey90", linetype=2), # y axis grid line
+        panel.background = element_rect(fill=NA, colour="black", size=0.5, linetype=1), # plot panel area setting
+        legend.background = element_rect(fill="white",colour="black"), # legend box setting
+        legend.position = "bottom", # position of the legend: c(x,y) OR "top","bottom","left","right"
+        legend.title=element_blank(), # Setting of the legend title
+        legend.margin = margin(t=0.01,r=0.2,b=0.1,l=0.1,"cm"), # margins for legend box
+        legend.key = element_rect(fill=NA,colour=NA), # legend keys settings
+        legend.key.width = unit(1.2, "cm"), # size(width) of the legend keys
+        strip.background = element_rect(fill=NA,colour=NA) # Setting of the facetted title area
+        )
+  return(gt)
+}
